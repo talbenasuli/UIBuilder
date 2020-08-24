c9b431f5444f5ae4f0e5ab57d3153015f92cd882
Tal Ben Asuli MAC <talbapepper@gmail.com>
2020-08-24 14:58:14 +0300

    Initial Commit

---
+++ UIBuilder/UIBuilder.h
@@ -0,0 +1,19 @@
+//
+//  UIBuilder.h
+//  UIBuilder
+//
+//  Created by Tal Ben Asuli MAC  on 24/08/2020.
+//  Copyright © 2020 UIBuider. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+//! Project version number for UIBuilder.
+FOUNDATION_EXPORT double UIBuilderVersionNumber;
+
+//! Project version string for UIBuilder.
+FOUNDATION_EXPORT const unsigned char UIBuilderVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <UIBuilder/PublicHeader.h>
+
+
